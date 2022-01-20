24d2dd102d2bc4be4d976bc16d99a786b0a905ce
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-20 21:00:44 +0700

    add task service

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,30 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { TaskStatus } from './task.model';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
