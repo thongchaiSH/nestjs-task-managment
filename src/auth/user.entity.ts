f0dfd06e731c5ea61e3f4f44dd44d55c06255b19
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-20 22:08:23 +0700

    add user module

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
