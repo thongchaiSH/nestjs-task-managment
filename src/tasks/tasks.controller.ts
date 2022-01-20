9732b590d0511d2a6a98942c7b98288aac6857e5
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-20 17:35:49 +0700

    add controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,19 +6,27 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    //if we have any filters defined, call tasksService.getTasksWilFilters
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      //otherwis, just get all tasks
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,12 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { search, status } = filterDto;
+    //define a temporary array to hold the result
+    let result = this.getAllTasks();
+    // do filter search
+    if (search) {
+      result = result.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+    // do filter status
+    if (status) {
+      result = result.filter((task) => task.status === status);
+    }
+    //return final result
+    return result;
   }
 
   getTaskById(id: string): Task {
