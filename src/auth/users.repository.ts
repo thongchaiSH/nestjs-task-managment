a627d1c4d23567516ea8343b533c7284379ed8a4
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-21 11:15:46 +0700

    add authen

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,15 +1,36 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log('salt', salt);
+    // console.log('hashedPassword', hashedPassword);
+
     const user = this.create({
       username,
-      password,
+      password:hashedPassword,
     });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      //duplicate username
+      console.log(error.code);
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exsits');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
