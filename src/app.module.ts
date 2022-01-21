4fba488edfc62e6f9a451f16ea860802194a9251
Thongchai Sidthikhattkorn - THA <ThongchS@th.fwd.com>
2022-01-21 16:06:28 +0700

    add config validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: ConfigValidationSchema,
     }),
     TasksModule,
     TypeOrmModule.forRootAsync({
@@ -18,11 +20,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
-          host: configService.get("DB_HOST"),
-          port: configService.get("DB_PORT"),
-          username: configService.get("DB_USERNAME"),
-          password: configService.get("DB_PASSWORD"),
-          database: configService.get("DB_DATABASE"),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
         };
       },
     }),
